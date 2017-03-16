77a423804d7fddc8515d07e283a907f118446531
bait-sli <bkaka15.09@hotmail.fr>
2017-03-16 09:14:43 +0100

    Update fdf.h

--- includes/fdf.h
+++ includes/fdf.h
@@ -38,73 +38,73 @@
 
 typedef struct		s_point
 {
-	int				x;
-	int				y;
-	int				z;
-}					t_point;
+	int		x;
+	int		y;
+	int		z;
+}			t_point;
 
 typedef struct		s_image
 {
-	int				bpp;
-	int				endian;
-	int				sizeline;
-	void			*img_ptr;
-	char			*data;
-}					t_image;
+	int		bpp;
+	int		endian;
+	int		sizeline;
+	void		*img_ptr;
+	char		*data;
+}			t_image;
 
 typedef struct		s_draw
 {
-	int				dx;
-	int				sx;
-	int				dy;
-	int				sy;
-	int				err;
-	int				e2;
-}					t_draw;
+	int		dx;
+	int		sx;
+	int		dy;
+	int		sy;
+	int		err;
+	int		e2;
+}			t_draw;
 
 typedef struct		s_newpos
 {
-	float			rx;
-	float			ry;
-	float			rz;
-	int				new_pos_x;
-	int				new_pos_y;
-	float			new_init_x;
-	float			new_init_y;
-	float			new_sub_widht;
-	float			new_sub_height;
-	float			grid;
-}					t_newpos;
+	float		rx;
+	float		ry;
+	float		rz;
+	int		new_pos_x;
+	int		new_pos_y;
+	float		new_init_x;
+	float		new_init_y;
+	float		new_sub_widht;
+	float		new_sub_height;
+	float		grid;
+}			t_newpos;
 
 typedef struct		s_param
 {
-	char			**ret;
-	int				endl;
-	int				len;
-	void			*mlx;
-	void			*window;
-	int				**nbr;
-	t_point			**dot;
-	t_image			pict;
-	t_newpos		**stock;
+	char		**ret;
+	int		endl;
+	int		len;
+	void		*mlx;
+	void		*window;
+	int		**nbr;
+	t_point		**dot;
+	t_image		pict;
+	t_newpos	**stock;
 	unsigned long	**color;
-}					t_param;
+}			t_param;
 
-void				print_menu(void *mlx, void *win);
-char				**read_file(char *file);
-int					size_line(char *str);
-int					check_file(char **stock);
-int					nb_line(char **stock);
-int					**ft_getdata(int endl, int size, char **stock);
-t_point				**get_map(int endl, int len, int **alpha);
-t_newpos			**get_pos_data(int endl, int size, t_point **data);
-void				print_map(t_param *s, unsigned long **color,
+void			print_menu(void *mlx, void *win);
+char			**read_file(char *file);
+int			size_line(char *str);
+int			check_file(char **stock);
+int			nb_line(char **stock);
+int			**ft_getdata(int endl, int size, char **stock);
+t_point			**get_map(int endl, int len, int **alpha);
+t_newpos		**get_pos_data(int endl, int size, t_point **data);
+void			print_map(t_param *s, unsigned long **color,
 						t_newpos **data);
-int					ft_atoi_base(char *str, int base);
+int			ft_atoi_base(char *str, int base);
 unsigned long		**get_color_data(int endl, int size, t_point **alpha,
 						char **stock);
-char				*clean_str(char *str);
-char				*remove_prefix(char *str);
-void				put_pixel(t_param *s, unsigned long color, int x, int y);
+char			*clean_str(char *str);
+char			*remove_prefix(char *str);
+void			put_pixel(t_param *s, unsigned long color, int x, int y);
 
 #endif
